c7e9d4057fa0aaf44e22a7b0e796ac385131b058
wangtao <borderpanda@163.com>
2021-06-22 15:47:50 +0800

    11

--- file.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-module.exports = (message) => {
- return new Promise((resolve, reject) => {
-  fs.appendFile("message.txt", `${message}\n`, (err) => {
-   err ? reject() : resolve();
-  });
- });
-};
